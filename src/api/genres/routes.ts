81270369e67f3f5ca76cb4dec75120c1a8ccba57
JIdayyy <julien_abbadie@hotmail.fr>
2022-10-25 12:16:51 +0200

    genre validator

--- src/api/genres/routes.ts
+++ src/api/genres/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
+import genreValidator from "../../JOI/genre";
+import bodyValidator from "../../middlewares/bodyValidator";
 import controller from "./controller";
 
 const router = Router();
 
 router.get("/", controller.getAll);
 router.get("/:id", controller.getOne);
-router.put("/:id", controller.put);
+router.put("/:id", bodyValidator(genreValidator.put), controller.put);
 router.delete("/:id", controller.delete);
-router.post("/", controller.post);
+router.post("/", bodyValidator(genreValidator.post), controller.post);
 
 export default router;
